9b30d8360a41e5edaf1617866fdfd1e571f267ba
Hassan Mansoor <hassan.mansoor96@gmail.com>
2022-06-14 12:47:37 +0200

    gallery updated description

--- React_Projects/gallery/src/portfolio-gallery/Data.js
+++ React_Projects/gallery/src/portfolio-gallery/Data.js
@@ -1,123 +1,112 @@
-const data =[
-    {
-        id:1,
-        image:"./images/html.jpg",
-        title:"HTML Tutoril",
-        description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
-        category:"frontend",
-       
-    },
-    {
-        id:2,
-        image:"./images/css.jpg",
-        title:"CSS Tutorial",
-        description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
-        category:"frontend",
-       
-    },
-    {
-        id:3,
-        image:"./images/javascript.jpg",
-        title:"JS Tutorial",
-        description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
-        category:"frontend",
-       
-    },
-    {
-        id:4,
-        image:"./images/react.jpg",
-        title:"React Js Tutorial",
-        description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
-        category:"frontend",
-       
-    },
-    {
-        id:5,
-        image:"./images/bootstrap.jpg",
-        title:"Bootstrap Tutorial",
-        description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
-        category:"frontend",
-       
-    },
-    {
-        id:6,
-        image:"./images/php.jpg",
-        title:"PHP Tutorial",
-        description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
-        category:"backend",
-       
-    },
-    {
-        id:7,
-        image:"./images/node-js.jpg",
-        title:"Node.js Tutorial",
-        description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
-        category:"backend",
-       
-    },
-    {
-        id:8,
-        image:"./images/asp.jpg",
-        title:"ASP Tutorial",
-        description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
-        category:"backend",
-       
-    },
-    {
-        id:9,
-        image:"./images/sql.jpg",
-        title:"SQL Tutorial",
-        description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
-        category:"database",
-       
-    },
-    {
-        id:10,
-        image:"./images/mongodb.jpg",
-        title:"Mongo DB Tutorial",
-        description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
-        category:"database",
-       
-    },
-    {
-        id:11,
-        image:"./images/c.jpg",
-        title:"C Tutorial",
-        description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
-        category:"programming",
-       
-    },
-    {
-        id:12,
-        image:"./images/c++.jpg",
-        title:"C++ Tutorial",
-        description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
-        category:"programming",
-       
-    },
-    {
-        id:13,
-        image:"./images/java.jpg",
-        title:"Java Tutorial",
-        description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
-        category:"programming",
-       
-    },
-    {
-        id:14,
-        image:"./images/python.jpg",
-        title:"Python Tutorial",
-        description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
-        category:"programming",
-       
-    },
-    {
-        id:15,
-        image:"./images/go.jpg",
-        title:"Go Tutorial",
-        description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-        category:"programming",
-       
-    },
+const data = [
+  {
+    id: 1,
+    image: './images/html.jpg',
+    title: 'HTML Tutorial',
+    description:
+      'HTML is the standard markup language for Web pages. With HTML you can create your own Website. HTML is easy to learn - You will enjoy it! ',
+    category: 'frontend',
+  },
+  {
+    id: 2,
+    image: './images/css.jpg',
+    title: 'CSS Tutorial',
+    description:
+      'CSS is used to define styles for your web pages, including the design, layout and variations in display for different devices and screen sizes. ',
+    category: 'frontend',
+  },
+  {
+    id: 3,
+    image: './images/javascript.jpg',
+    title: 'JS Tutorial',
+    description:
+      "JavaScript is a dynamic programming language that's used for web development, in web applications, for game development, and lots more. It allows you to implement dynamic features on web pages that cannot be done with only HTML and CSS. ",
+    category: 'frontend',
+  },
+  {
+    id: 4,
+    image: './images/react.jpg',
+    title: 'React Js Tutorial',
+    description:
+      'React is a declarative, efficient, and flexible JavaScript library for building user interfaces. It lets you compose complex UIs from small and isolated pieces of code called “components”. ',
+    category: 'frontend',
+  },
+  {
+    id: 5,
+    image: './images/bootstrap.jpg',
+    title: 'Bootstrap Tutorial',
+    description: 'Bootstrap is a potent front-end framework used to create modern websites and web apps. It is open-source and free to use, yet features numerous HTML and CSS templates for UI interface elements such as buttons and forms. Bootstrap also supports JavaScript extensions. ',
+    category: 'frontend',
+  },
+  {
+    id: 6,
+    image: './images/php.jpg',
+    title: 'PHP Tutorial',
+    description: '(PHP: Hypertext Preprocessor) An extremely popular scripting language that is used to create dynamic Web pages. Combining syntax from the C, Java and Perl languages, PHP code is embedded within HTML pages for server side execution. ',
+    category: 'backend',
+  },
+  {
+    id: 7,
+    image: './images/node-js.jpg',
+    title: 'Node.js Tutorial',
+    description: 'Node. js (Node) is an open source development platform for executing JavaScript code server-side. Node is useful for developing applications that require a persistent connection from the browser to the server and is often used for real-time applications such as chat, news feeds and web push notifications. ',
+    category: 'backend',
+  },
+  {
+    id: 8,
+    image: './images/asp.jpg',
+    title: 'ASP Tutorial',
+    description: 'Active Server Pages (ASP) enables web servers to dynamically generate webpages and create interactive web applications by using server-side scripting technology. ',
+    category: 'backend',
+  },
+  {
+    id: 9,
+    image: './images/sql.jpg',
+    title: 'SQL Tutorial',
+    description: 'Structured Query Language (SQL) is a standardized programming language that is used to manage relational databases and perform various operations on the data in them. ',
+    category: 'database',
+  },
+  {
+    id: 10,
+    image: './images/mongodb.jpg',
+    title: 'Mongo DB Tutorial',
+    description: 'MongoDB is an open source NoSQL database management program. NoSQL is used as an alternative to traditional relational databases. NoSQL databases are quite useful for working with large sets of distributed data. MongoDB is a tool that can manage document-oriented information, store or retrieve information. ',
+    category: 'database',
+  },
+  {
+    id: 11,
+    image: './images/c.jpg',
+    title: 'C Tutorial',
+    description: 'C is a high-level and general-purpose programming language that is ideal for developing firmware or portable applications. ',
+    category: 'programming',
+  },
+  {
+    id: 12,
+    image: './images/c++.jpg',
+    title: 'C++ Tutorial',
+    description: 'C++ is an object-oriented programming (OOP) language that is viewed by many as the best language for creating large-scale applications. C++ is a superset of the C language. ',
+    category: 'programming',
+  },
+  {
+    id: 13,
+    image: './images/java.jpg',
+    title: 'Java Tutorial',
+    description: 'Java is an object-oriented programming language that produces software for multiple platforms. When a programmer writes a Java application, the compiled code (known as bytecode) runs on most operating systems (OS), including Windows, Linux and Mac OS. ',
+    category: 'programming',
+  },
+  {
+    id: 14,
+    image: './images/python.jpg',
+    title: 'Python Tutorial',
+    description: 'Python is a computer programming language often used to build websites and software, automate tasks, and conduct data analysis. Python is a general-purpose language, meaning it can be used to create a variety of different programs and is not specialized for any specific problems. ',
+    category: 'programming',
+  },
+  {
+    id: 15,
+    image: './images/go.jpg',
+    title: 'Go Tutorial',
+    description: 'Go (also called Golang or Go language) is an open source programming language used for general purpose. Go was developed by Google engineers to create dependable and efficient software. Most similarly modeled after C, Go is statically typed and explicit. ',
+    category: 'programming',
+  },
 ]
-export default data;
+export default data
